9b667e8e14c86b5d2e5b4f7bb84dc8a5a728963f
Jaap Jan Meijer <jjmeijer88@gmail.com>
2016-06-04 20:40:13 +0200

    Update license and log tag

--- power.c
+++ power.c
@@ -1,17 +1,14 @@
 /*
- * Copyright (C) 2012 The Android Open Source Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Copyright (C) 2016 The Android-x86 Open Source Project
+ *
+ * Licensed under the GNU General Public License Version 2 or later.
+ * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.gnu.org/licenses/gpl.html
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *
  */
 #include <errno.h>
 #include <string.h>
@@ -19,7 +16,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define LOG_TAG "Legacy PowerHAL"
+#define LOG_TAG "PowerHAL"
 #include <utils/Log.h>
 
 #include <hardware/hardware.h>
