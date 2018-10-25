6c1499e8eeb64a7cc36eef90dcc5dae092640828
lambdadroid <lambdadroid@gmail.com>
2018-10-25 19:53:01 +0200

    power: Use . instead of - as separator for device specific HALs

--- Power.cpp
+++ Power.cpp
@@ -1,4 +1,4 @@
-#define LOG_TAG "android.hardware.power@1.0-service-x86"
+#define LOG_TAG "android.hardware.power@1.0-service.x86"
 
 #include <android-base/file.h>
 #include "Power.h"
--- service.cpp
+++ service.cpp
@@ -1,4 +1,4 @@
-#define LOG_TAG "android.hardware.power@1.0-service-x86"
+#define LOG_TAG "android.hardware.power@1.0-service.x86"
 
 #include <hidl/HidlTransportSupport.h>
 #include "Power.h"
